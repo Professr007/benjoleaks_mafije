785764b84ce20b597d77105d2ec29459e3aacfbc
Professr <71907066+Professr007@users.noreply.github.com>
2022-06-12 08:31:13 +0100

    GetPlayerId(-1)-PlayerPedId(),DisableControlAction

--- klijent_s/f6meni.lua
+++ klijent_s/f6meni.lua
@@ -136,7 +136,7 @@ RegisterNetEvent('vaskezzyy:vezivanje')
 AddEventHandler('vaskezzyy:vezivanje', function()
 
   IsHandcuffed    = not IsHandcuffed;
-  local playerPed = GetPlayerPed(-1)
+  local playerPed = PlayerPedId()
 
   Citizen.CreateThread(function()
 
@@ -171,10 +171,10 @@ Citizen.CreateThread(function()
 	  if IsHandcuffed then
 		if IsDragged then
 		  local ped = GetPlayerPed(GetPlayerFromServerId(CopPed))
-		  local myped = GetPlayerPed(-1)
+		  local myped = PlayerPedId()
 		  AttachEntityToEntity(myped, ped, 11816, 0.54, 0.54, 0.0, 0.0, 0.0, 0.0, false, false, false, false, 2, true)
 		else
-		  DetachEntity(GetPlayerPed(-1), true, false)
+		  DetachEntity(PlayerPedId(), true, false)
 		end
 	  end
 	end
@@ -184,9 +184,42 @@ Citizen.CreateThread(function()
 	while true do
 	  Wait(0)
 	  if IsHandcuffed then
-		DisableControlAction(0, 142, true) -- MeleeAttackAlternate
-		DisableControlAction(0, 30,  true) -- MoveLeftRight
-		DisableControlAction(0, 31,  true) -- MoveUpDown
-	  end
+			DisableControlAction(0, 1, true) -- Disable pan
+			DisableControlAction(0, 2, true) -- Disable tilt
+			DisableControlAction(0, 24, true) -- Attack
+			DisableControlAction(0, 257, true) -- Attack 2
+			DisableControlAction(0, 25, true) -- Aim
+			DisableControlAction(0, 263, true) -- Melee Attack 1
+			DisableControlAction(0, 32, true) -- W
+			DisableControlAction(0, 34, true) -- A
+			DisableControlAction(0, 31, true) -- S
+			DisableControlAction(0, 30, true) -- D
+			DisableControlAction(0, 45, true) -- Reload
+			DisableControlAction(0, 22, true) -- Jump
+			DisableControlAction(0, 44, true) -- Cover
+			DisableControlAction(0, 37, true) -- Select Weapon
+			DisableControlAction(0, 23, true) -- Also 'enter'?
+			DisableControlAction(0, 288,  true) -- Disable phone
+			DisableControlAction(0, 289, true) -- Inventory
+			DisableControlAction(0, 170, true) -- Animations
+			DisableControlAction(0, 167, true) -- Job
+			DisableControlAction(0, 0, true) -- Disable changing view
+			DisableControlAction(0, 26, true) -- Disable looking behind
+			DisableControlAction(0, 73, true) -- Disable clearing animation
+			DisableControlAction(2, 199, true) -- Disable pause screen
+			DisableControlAction(0, 59, true) -- Disable steering in vehicle
+			DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
+			DisableControlAction(0, 72, true) -- Disable reversing in vehicle
+			DisableControlAction(2, 36, true) -- Disable going stealth
+			DisableControlAction(0, 47, true)  -- Disable weapon
+			DisableControlAction(0, 264, true) -- Disable melee
+			DisableControlAction(0, 257, true) -- Disable melee
+			DisableControlAction(0, 140, true) -- Disable melee
+			DisableControlAction(0, 141, true) -- Disable melee
+			DisableControlAction(0, 142, true) -- Disable melee
+			DisableControlAction(0, 143, true) -- Disable melee
+			DisableControlAction(0, 75, true)  -- Disable exit vehicle
+			DisableControlAction(27, 75, true) -- Disable exit vehicle
+	  	end
 	end
 end)
